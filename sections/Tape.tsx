8253873d88b2f22326d060c14f1cfbdd47f9fcab
Arthur Laroya <arthur.j.laroya@gmail.com>
2026-01-13 12:10:28 +1100

    feat: add Tape and Testimonials sections with dynamic content; update imports for assets

--- sections/Tape.tsx
+++ sections/Tape.tsx
@@ -1,3 +1,35 @@
+import StarIcon from "@/public/icons/star.svg";
+
+const words = [
+  "Performant",
+  "Accessible",
+  "Interactive",
+  "User Friendly",
+  "Responsive",
+  "Intuitive",
+  "Dynamic",
+  "Engaging",
+  "Aesthetic",
+  "Beautiful",
+];
+
 export const TapeSection = () => {
-  return <div>Tape Section</div>;
+  return (
+    <div className="py-16 lg:py-24 overflow-x-clip">
+      <div className="bg-linear-to-r from-emerald-300 to-pink-400 -rotate-3 -mx-1">
+        <div className="flex mask-[linear-gradient(to_right,transparent,black_10%,black_90%,transparent)]">
+          <div className="flex flex-none gap-4 py-3">
+            {words.map((word, index) => (
+              <div key={index} className="inline-flex gap-4 items-center">
+                <span className="text-gray-900 uppercase font-extrabold tracking-tight text-sm">
+                  {word}
+                </span>
+                <StarIcon className="size-6 text-gray-900 -rotate-12" />
+              </div>
+            ))}
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
--- sections/Testimonials.tsx
+++ sections/Testimonials.tsx
@@ -1,8 +1,8 @@
-import memojiAvatar1 from "@/assets/images/memoji-avatar-1.png";
-import memojiAvatar2 from "@/assets/images/memoji-avatar-2.png";
-import memojiAvatar3 from "@/assets/images/memoji-avatar-3.png";
-import memojiAvatar4 from "@/assets/images/memoji-avatar-4.png";
-import memojiAvatar5 from "@/assets/images/memoji-avatar-5.png";
+import memojiAvatar1 from "@/public/images/memoji-avatar-1.png";
+import memojiAvatar2 from "@/public/images/memoji-avatar-2.png";
+import memojiAvatar3 from "@/public/images/memoji-avatar-3.png";
+import memojiAvatar4 from "@/public/images/memoji-avatar-4.png";
+import memojiAvatar5 from "@/public/images/memoji-avatar-5.png";
 
 const testimonials = [
   {
@@ -40,3 +40,5 @@ const testimonials = [
 export const TestimonialsSection = () => {
   return <div>Testimonials Section</div>;
 };
+
+// 2:15:46
