e05d513bdafc3beca99e1575f71d8a38425f5719
Arthur Laroya <arthur.j.laroya@gmail.com>
2026-01-16 12:11:30 +1100

    feat: enhance Contact and Footer sections with new layout and content; add dynamic footer links

--- sections/Contact.tsx
+++ sections/Contact.tsx
@@ -1,5 +1,34 @@
-export const ContactSection = () => {
-  return <div>Contact Section</div>;
-};
+import ArrowUpRightIcon from "@/public/icons/arrow-up-right.svg";
+import grainImage from "@/public/images/grain.jpg";
 
-// 3:55:37
+export const ContactSection = () => {
+  return (
+    <div className="py-16 pt-12 lg:py-24 lg:pt-20">
+      <div className="container">
+        <div className="bg-linear-to-r from-emerald-300 to-pink-300 text-gray-900 py-8 px-10 rounded-3xl text-center md:text-left relative overflow-hidden z-0">
+          <div
+            className="absolute inset-0 opacity-5 -z-10"
+            style={{ backgroundImage: `url(${grainImage.src})` }}
+          />
+          <div className="flex flex-col md:flex-row gap-8 md:gap-16 items-center">
+            <div>
+              <h2 className="font-semibold text-2xl md:text-3xl tracking-tight">
+                Let's design something creative together
+              </h2>
+              <p className="text-sm md:text-base mt-2">
+                Ready to bring your ideas to life? Get in touch with me today
+                and I can help you achieve your goals
+              </p>
+            </div>
+            <div>
+              <button className="text-white bg-gray-900 border border-gray-950 inline-flex items-center gap-2 px-6 h-12 rounded-full w-max">
+                <span className="font-semibold">Contact Me</span>
+                <ArrowUpRightIcon className="size-4" />
+              </button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
--- sections/Footer.tsx
+++ sections/Footer.tsx
@@ -1,3 +1,35 @@
+import ArrowUpRightIcon from "@/public/icons/arrow-up-right.svg";
+
+const footerLinks = [
+  { name: "Twitter", url: "http://" },
+  { name: "Instagram", url: "http://" },
+  { name: "Behanced", url: "http://" },
+  { name: "LinkedIn", url: "http://" },
+];
+
 export const Footer = () => {
-  return <div>Footer Section</div>;
+  return (
+    <footer className="relative -z-10 overflow-x-clip">
+      <div className="absolute h-100 w-400 bottom-0 left-1/2 -translate-x-1/2 bg-emerald-300/50 mask-[radial-gradient(50%_50%_at_bottom_center,black,transparent)] -z-10" />
+      <div className="container">
+        <div className="border-t border-black/15 py-6 text-sm flex flex-col md:flex-row md:justify-between items-center gap-8">
+          <div className="text-black/40">&copy; 2026. All rights reserved</div>
+          <nav className="flex flex-col md:flex-row  items-center gap-8">
+            {footerLinks.map((link) => (
+              <a
+                key={link.name}
+                href={link.url}
+                target="_blank"
+                rel="noopener noreferrer"
+                className="inline-flex items-center gap-1.5"
+              >
+                <span className="font-semibold">{link.name}</span>
+                <ArrowUpRightIcon className="size-4" />
+              </a>
+            ))}
+          </nav>
+        </div>
+      </div>
+    </footer>
+  );
 };
