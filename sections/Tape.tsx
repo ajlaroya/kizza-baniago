f65bb24cedcfa3ea4736128931e8d62c15744edb
Arthur Laroya <arthur.j.laroya@gmail.com>
2026-01-19 11:14:28 +1100

    feat: update TapeSection with new animation and modify word list; adjust background gradient for improved aesthetics

--- sections/Tape.tsx
+++ sections/Tape.tsx
@@ -1,7 +1,8 @@
 import StarIcon from "@/public/icons/star.svg";
+import { Fragment } from "react/jsx-runtime";
 
 const words = [
-  "Performant",
+  "Creative",
   "Accessible",
   "Interactive",
   "User Friendly",
@@ -16,16 +17,20 @@ const words = [
 export const TapeSection = () => {
   return (
     <div className="py-16 lg:py-24 overflow-x-clip">
-      <div className="bg-linear-to-r from-emerald-300 to-pink-400 -rotate-3 -mx-1">
+      <div className="bg-linear-to-r from-emerald-300 to-pink-300 -rotate-3 -mx-1">
         <div className="flex mask-[linear-gradient(to_right,transparent,black_10%,black_90%,transparent)]">
-          <div className="flex flex-none gap-4 py-3">
-            {words.map((word, index) => (
-              <div key={index} className="inline-flex gap-4 items-center">
-                <span className="text-gray-900 uppercase font-extrabold tracking-tight text-sm">
-                  {word}
-                </span>
-                <StarIcon className="size-6 text-gray-900 -rotate-12" />
-              </div>
+          <div className="flex flex-none gap-4 pr-4 py-3 animate-move-left [animation-duration:30s]">
+            {[...new Array(2)].fill(0).map((_, index) => (
+              <Fragment key={index}>
+                {words.map((word, index) => (
+                  <div key={index} className="inline-flex gap-4 items-center">
+                    <span className="text-gray-900 uppercase font-extrabold tracking-tight text-sm">
+                      {word}
+                    </span>
+                    <StarIcon className="size-6 text-gray-900 -rotate-12" />
+                  </div>
+                ))}
+              </Fragment>
             ))}
           </div>
         </div>
